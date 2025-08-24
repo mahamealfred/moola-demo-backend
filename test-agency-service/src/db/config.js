b312586e6760f6e848211b5abaa0e33975e449e3
mahamealfred <mahamealfred@gmail.com>
2025-08-24 12:04:18 +0200

    changes imples 12

--- test-agency-service/src/db/config.js
+++ test-agency-service/src/db/config.js
@@ -19,9 +19,9 @@ const sequelize = new Sequelize(
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log("✅ Database connection established successfully.");
+    console.log("Database connection established successfully.");
   } catch (error) {
-    console.error("❌ Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
 
